e94d4050576deaf38e92959b81aa424252994870
Hannes Hinrichs <hannes.hinrichs@senselab.io>
2018-11-27 18:05:11 +0100

    -Fixed bug with entfernung -Started Daydream conversion

---
+++ Assets/Scripts/BezierLineRenderer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierLineRenderer
+{
+
+    public LineRenderer lineRenderer = null;
+    bool cubic = false;
+
+    [Header("Use 3 Points for x^2 functions")]
+    private Transform point0;
+    private Transform point1;
+    private Transform point2;
+    int interpolationPoints;
+    private Vector3[] positions;
+    public bool isSet = false;
+
+    public BezierLineRenderer(LineRenderer _lineRenderer, int _interpolationPoints)
+    {
+        lineRenderer = _lineRenderer;
+        interpolationPoints = _interpolationPoints;
+        positions = new Vector3[interpolationPoints];
+        lineRenderer.positionCount = interpolationPoints;
+    }
+
+    public void setPositionCounts()
+    {
+        lineRenderer.positionCount = interpolationPoints;
+    }
+
+    public void DrawLinearCurve(Transform _point0, Transform _point1, Transform _point2, Transform _pointer)
+    {
+        point0 = _point0;
+        point1 = _point1;
+        point2 = _point2;
+        positions[0] = _pointer.position;
+        for (int i = 1; i < interpolationPoints; ++i)
+        {
+            float t = i / (float)interpolationPoints;
+            positions[i] = CalculateLinearBezierPoint(t, CalculateLinearBezierPoint(t, point0.position, point1.position), CalculateLinearBezierPoint(t, point1.position, point2.position));
+        }
+        setPositionCounts();
+        lineRenderer.SetPositions(positions);
+    }
+
+    private Vector3 CalculateLinearBezierPoint(float t, Vector3 p0, Vector3 p1)
+    {
+        return p0 + t * (p1 - p0);
+    }
+
+
+}
---
+++ Assets/Scripts/VRBezierRaycasterDaydream.cs
@@ -0,0 +1,346 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class VRBezierRaycasterDaydream : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class Callback : UnityEvent<Ray, RaycastHit> { }
+
+    [Header("Settings")]
+    [Tooltip("Maximum distance the ray gets casted")]
+    public float maxRayDistance = 500.0f;
+    [Tooltip("Number of interpolation points within the line. The more points the more overhead")]
+    public int interpolationPoints;
+    [Tooltip("Value between 0 and 100. Closer to 100 increases bending factor")]
+    public float bezierPoint2PercentualPosition = 50;
+    // How fast does the object gets away from me
+    [Tooltip("Sensibility of touchpad swipe. Higher value increases sensibility")]
+    public float zoomFactor = 3;
+    // Offset the Grabed Object is close to me. It is recommended to set the factor that the Asset does not get clipped into the Camera
+    [Tooltip("Offset from near Object to controller")]
+    public float closeOffset = 0.0f;
+    [Header("Modes")]
+    [Tooltip("Enable editor Testing. 'e' Button for anchoring the Object")]
+    public bool testMode = false;
+    [Tooltip("Enables rotation of the Object")]
+    public bool enableRotation = false;
+
+    [Header("Prefabs")]
+    [Tooltip("Prefab for the attachment Point")]
+    public GameObject anglePointPrefab;
+    [Tooltip("Prefab for the bezierPoint2 - 'bending Around Point'")]
+    public GameObject bezierPoint2Prefab;
+    [Tooltip("Prefab for the desired cursor")]
+    public GameObject hitPointCursorPrefab;
+    [Tooltip("The LineRenderer instance of the GO")]
+    public LineRenderer lineRenderer = null;
+
+    private Transform controllerAnchor = null;
+    private Transform hitObject;
+    private BezierLineRenderer bezierRenderer;
+    public LayerMask excludeLayers;
+    public VRBezierRaycasterDaydream.Callback raycastHitCallback;
+    
+    private GameObject anglePointPrefabInstance;
+    private GameObject hitPointCursorPrefabInstance;
+    private GameObject bezierPoint2;
+
+    private bool objectIsAttached = false;
+
+    private bool tap, swipeLeft, swipeRight, swipeUp, swipeDown;
+    private bool isDragging;
+    private Vector2 startTouch, swipeDelta;
+
+    private Vector3 colliderSize;
+
+    private Vector2 oldPosition, newPosition;
+    private Quaternion oldRotation = new Quaternion(0, 0, 0, 0);
+    private Vector3 desigredRotation;
+
+    private Rigidbody hitObjectRigidbody;
+
+
+
+    void Awake()
+    {
+        if (controllerAnchor == null)
+        {
+            Debug.LogWarning("Assign controller");
+            GameObject left = GameObject.Find("controllerVisual");
+            if (left != null)
+            {
+                controllerAnchor = left.transform;
+            }
+        }
+        
+        if (lineRenderer == null)
+        {
+            Debug.LogWarning("Assign a line renderer in the inspector!");
+            lineRenderer = gameObject.AddComponent<LineRenderer>();
+            lineRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+            lineRenderer.receiveShadows = false;
+            lineRenderer.widthMultiplier = 0.02f;
+        }
+    }
+
+    void Start()
+    {
+        Color c1 = Color.white;
+        Color c2 = new Color(1, 1, 1, 1);
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.startColor = c2;
+        lineRenderer.endColor = c1;
+        lineRenderer.startWidth = 0.017f;
+        lineRenderer.endWidth = 0.005f;
+        Transform pointer = Pointer;
+        bezierRenderer = new BezierLineRenderer(lineRenderer, interpolationPoints);
+
+        oldRotation = this.transform.rotation;
+    }
+
+    GvrControllerInputDevice ControllerInputDevice { get; set; }
+
+    Transform Pointer
+    {
+        get
+        {
+            if (ControllerInputDevice != null)
+            {
+                return controllerAnchor;
+            }
+            Debug.Log("No Controller found ...");
+            return null;
+        }
+    }
+
+    private void Reset()
+    {
+        oldPosition = startTouch;
+        startTouch = swipeDelta = new Vector2(0f, 0f);
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        Debug.Log(ControllerInputDevice.GetButton(GvrControllerButton.TouchPadButton));
+        //this is just for editor testing
+        Transform pointer;
+        if (testMode)
+            pointer = this.transform;
+        else
+            pointer = Pointer;
+
+        if (pointer == null)
+        {
+            return;
+        }
+        #region TouchControls
+
+        tap = swipeLeft = swipeRight = swipeUp = swipeDown = false;
+        
+        if (OVRInput.Get(OVRInput.Touch.One))
+        {
+            startTouch = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+            if (!isDragging)
+            {
+                oldPosition = startTouch;
+            }
+            isDragging = true;
+        }
+        if (OVRInput.GetUp(OVRInput.Touch.One))
+        {
+            isDragging = false;
+            Reset();
+        }
+
+        swipeDelta = new Vector2(0f, 0f);
+
+        // Distance Calculation
+        if(isDragging)
+        {
+            if (OVRInput.Get(OVRInput.Touch.One))
+            {
+                swipeDelta = startTouch - oldPosition;
+            }
+        }
+        
+        // Deadzone Crossing
+        if (swipeDelta.magnitude > 0f && hitObjectRigidbody != null)
+        {
+            // Which Direction?
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+            if(Mathf.Abs(x) > Mathf.Abs(y))
+            {
+                if (x < 0)
+                {
+                    swipeLeft = true;
+                }
+                   
+                else{
+                    swipeRight = true;
+                }
+                    
+            }
+            else
+            {
+                float predictedDistance = Vector3.Distance(transform.TransformPoint(anglePointPrefabInstance.transform.localPosition + new Vector3(0f, 0f, y) * zoomFactor), this.transform.position );
+                Vector3 predictedPosition = anglePointPrefabInstance.transform.localPosition + new Vector3(0f, 0f, y) * zoomFactor;
+
+                if (y < 0)
+                {
+                    swipeDown = true;
+                    if (anglePointPrefabInstance != null && hitObjectRigidbody != null)
+                    {
+
+                        if(predictedDistance >= (colliderSize.z/2) && predictedPosition.z > 0)
+                        {
+                            anglePointPrefabInstance.transform.localPosition += new Vector3(0f, 0f, y) * zoomFactor;
+                            bezierPoint2.transform.localPosition += new Vector3(0f, 0f, y * (bezierPoint2PercentualPosition / 100) ) * zoomFactor;
+                        }
+                        else
+                        {
+                            anglePointPrefabInstance.transform.localPosition = this.transform.localPosition + new Vector3(0f, 0f, (colliderSize.z / 2) + closeOffset);
+                            bezierPoint2.transform.localPosition = this.transform.localPosition + new Vector3(0f, 0f, anglePointPrefabInstance.transform.localPosition.z * (bezierPoint2PercentualPosition / 100) );
+                        }
+                               
+                    }
+
+                }
+                else
+                {
+                    swipeUp = true;
+                    if (anglePointPrefabInstance != null && hitObjectRigidbody != null)
+                    {
+                        if (predictedDistance >= (colliderSize.z / 2)+closeOffset && predictedPosition.z > 0)
+                        {
+                            anglePointPrefabInstance.transform.localPosition += new Vector3(0f, 0f, y) * zoomFactor;
+                            bezierPoint2.transform.localPosition += new Vector3(0f, 0f, y * (bezierPoint2PercentualPosition / 100)) * zoomFactor;
+                        }
+                        else
+                        {
+                            anglePointPrefabInstance.transform.localPosition = this.transform.localPosition + new Vector3(0f, 0f, (colliderSize.z / 2) + closeOffset);
+                            bezierPoint2.transform.localPosition = this.transform.localPosition + new Vector3(0f, 0f, anglePointPrefabInstance.transform.localPosition.z * (bezierPoint2PercentualPosition / 100));
+                        }
+                    }
+
+                }
+            }
+
+        Reset();
+        }
+        #endregion
+
+        #region AttachControls
+        if (objectIsAttached == false) {
+
+            Ray laserPointer = new Ray(pointer.position, pointer.forward);
+
+            if (lineRenderer != null)
+            {
+                lineRenderer.positionCount = 2;
+                lineRenderer.SetPosition(0, laserPointer.origin);
+                lineRenderer.SetPosition(1, laserPointer.origin + laserPointer.direction * maxRayDistance);
+            }
+
+
+            RaycastHit hit;
+            if (Physics.Raycast(laserPointer, out hit, maxRayDistance, ~excludeLayers))
+            {
+                if (lineRenderer != null)
+                {
+                    lineRenderer.SetPosition(1, hit.point);
+                }
+
+                 if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) == true && (hit.rigidbody != null) || (Input.GetKeyDown("e") && testMode ) )
+                {
+
+                    Debug.Log("Object hit, creating dependencies ...");
+                    objectIsAttached = true;
+                    hitObject = hit.transform;
+
+                    Debug.Log("Instantiate hitPointCursor and align it right...");
+                    hitPointCursorPrefabInstance = Instantiate(hitPointCursorPrefab, hit.point, Quaternion.identity);
+                    hitPointCursorPrefabInstance.transform.rotation = Quaternion.LookRotation(this.transform.position - hitPointCursorPrefab.transform.position);
+                    hitPointCursorPrefabInstance.transform.parent = hitObject;
+
+                    Debug.Log("Creating spring anchor...");
+                    anglePointPrefabInstance = Instantiate(anglePointPrefab, hit.point, Quaternion.identity);
+                    
+                    Debug.Log("Aligning Spring...");
+                    SpringJoint anglePointPrefabAnglePoint = anglePointPrefabInstance.GetComponent<SpringJoint>();
+                    anglePointPrefabAnglePoint.autoConfigureConnectedAnchor = false;
+                    anglePointPrefabAnglePoint.connectedBody = hitObject.GetComponent<Rigidbody>();
+                    anglePointPrefabAnglePoint.connectedAnchor = hitPointCursorPrefabInstance.transform.localPosition;
+                    anglePointPrefabAnglePoint.anchor = new Vector3(0f, 0f, 0f);
+                    anglePointPrefabInstance.transform.parent = this.transform;
+                    anglePointPrefabInstance.transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+
+
+                    Debug.Log("Calculating collider Box of the hitObject...");
+                    Collider collider = hitObject.GetComponent<Collider>();
+                    colliderSize = collider.bounds.size;
+                    Debug.Log("Size of collider " + colliderSize.z);
+
+                    Debug.Log("Creating bezierPoint2... ");
+                    bezierPoint2 = Instantiate(bezierPoint2Prefab, hit.point, Quaternion.identity);
+                    float lineLength = Vector3.Distance(this.transform.position, hit.point);
+                    Debug.Log("LineLength to Instantiate " + lineLength);
+                    bezierPoint2.transform.parent = this.transform;
+                    Vector3 originPosition = bezierPoint2.transform.localPosition;
+
+                    //Approaching from AnglePointPrefab ...
+                    bezierPoint2.transform.localPosition = bezierPoint2.transform.localPosition + new Vector3(0f, 0f, (-lineLength * (100-bezierPoint2PercentualPosition))/100);
+                    bezierPoint2.transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+
+                    Debug.Log("Get the rigidbody component...");
+                    hitObjectRigidbody = hitObject.GetComponent<Rigidbody>();
+                    
+                    Debug.Log("Tweaking the Rigidbody...");
+                    hitObjectRigidbody.useGravity = true;
+                    hitObjectRigidbody.drag = 10;
+                    hitObjectRigidbody.freezeRotation = true;
+
+                    oldRotation = this.transform.rotation;
+                }
+
+                if (raycastHitCallback != null)
+                {
+                    raycastHitCallback.Invoke(laserPointer, hit);
+                }
+            }
+        }
+        else
+        {
+            bezierRenderer.DrawLinearCurve(transform, bezierPoint2.transform, hitPointCursorPrefabInstance.transform, pointer);
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) == true && (hitObject != null) || (Input.GetKeyDown("e") && testMode))
+            {
+                Debug.Log("Removing anchor from object...");
+                hitObjectRigidbody.drag = 2;
+                hitObjectRigidbody.constraints = RigidbodyConstraints.None;
+                objectIsAttached = false;
+                hitObjectRigidbody.useGravity = true;
+                hitObject = null;
+                Destroy(bezierPoint2);
+                Destroy(anglePointPrefabInstance);
+                Destroy(hitPointCursorPrefabInstance);
+                hitObjectRigidbody = null;
+                lineRenderer.positionCount = 2;
+            }
+        }
+        #endregion
+    }
+
+    void FixedUpdate()
+    {
+
+        if(hitObjectRigidbody != null && enableRotation)
+        {
+            Quaternion deltaRotation = this.transform.rotation * Quaternion.Inverse(oldRotation);
+            hitObjectRigidbody.rotation = deltaRotation * hitObjectRigidbody.rotation;  
+            oldRotation = this.transform.rotation;
+        }
+    }
+}
+
